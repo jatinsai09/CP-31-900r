d1dfffd184259c133ff2acdb33c4efbb572e0471
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 23:25:04 +0530

    Add files via upload

---
+++ B_Odd_Grasshopper.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        long long x, n;
+        cin >> x >> n;
+        long long d;
+
+        switch (n % 4) {
+            case 0:
+                d = 0;
+                break;
+
+            case 1:
+                d = -n;
+                break;
+
+            case 2:
+                d = 1;
+                break;
+
+            case 3:
+                d = n + 1;
+                break;
+        }
+
+        if (x % 2) {
+            cout << x - d << endl;
+        }
+        else {
+            cout << x + d << endl;
+        }
+    }
+}
